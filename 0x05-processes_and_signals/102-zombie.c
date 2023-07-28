6c8647536b39d3449144099a1bd90fe323105121
moohamedmagdi <moohamedmagdi@gmail.com>
2023-07-28 23:20:59 +0000

    102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,26 +4,26 @@
 #include <sys/wait.h>
 
 /**
- *  * main - zombies
- *   *
- *    * Description: make five zombies
- *     * Return: 0 for success
- *      */
+ * * main - zombies
+ * *
+ * * Description: make five zombies
+ * * Return: 0 for success
+ * */
 int main(void)
 {
-		int i;
-			pid_t pidme;
+	int i;
+	pid_t pidme;
 
-				i = 0;
-					while (i < 5)
-							{
-										pidme = fork();
-												if (pidme)
-																printf("Zombie process created, PID: %i\n", pidme);
-														else
-																		exit(0);
-																i++;
-																	}
-						sleep(100);
-							return (0);
+	i = 0;
+	while (i < 5)
+	{
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
+	}
+	sleep(100);
+	return (0);
 }
